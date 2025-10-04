78312cfffa0ed9da7571fa9c8546d76acb6a3065
ilhamsqa <hstorereisback@gmail.com>
2025-10-04 21:07:42 +0700

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -21,7 +21,7 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing bot
             </li>
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
